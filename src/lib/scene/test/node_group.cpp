376ac9ce010b5f576f2bac60018bd5ef7fa963fb
jan p springer <jsd@igroup.org>
2014-04-10 16:32:01 +0100

    added: unit tests for grp/xform

---
+++ src/lib/scene/test/node_group.cpp
@@ -0,0 +1,71 @@
+// -*- Mode:C++ -*-
+
+/**************************************************************************************************/
+/*                                                                                                */
+/* Copyright (C) 2014 University of Hull                                                          */
+/*                                                                                                */
+/**************************************************************************************************/
+/*                                                                                                */
+/*  module     :  scene/test/node_group.cpp                                                       */
+/*  project    :                                                                                  */
+/*  description:                                                                                  */
+/*                                                                                                */
+/**************************************************************************************************/
+
+// includes, system
+
+//#include <>
+
+// includes, project
+
+#include <scene/node/group.hpp>
+
+// internal unnamed namespace
+
+namespace {
+  
+  // types, internal (class, enum, struct, union, typedef)
+
+  // variables, internal
+  
+  // functions, internal
+
+} // namespace {
+
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(node_group_ctor)
+{
+  using namespace scene::node;
+  
+  group const g("group");
+  
+  BOOST_CHECK(true == g.children.get().empty());
+  BOOST_MESSAGE(g << '\n');
+}
+
+BOOST_AUTO_TEST_CASE(node_group_set_children)
+{
+  using namespace scene::node;
+  
+  group  g0("grp0");
+  group* g1(new group("grp1"));
+  group* g2(new group("grp2"));
+
+  BOOST_CHECK(true == g0.children.get().empty());
+  
+  g0.children.set({ g1, g2 });
+  
+  BOOST_CHECK(2 == g0.children.get().size());
+  BOOST_MESSAGE(g0 << '\n' << *g1 << '\n' << *g2 << '\n');
+  
+  group* g3(new group("grp3"));
+  group* g4(new group("grp4"));
+  
+  auto const old(g0.children.set({ g3, g4 }));
+  BOOST_MESSAGE(g0 << '\n' << *g1 << '\n' << *g2 << '\n' << *g3 << '\n' << *g4 << '\n');
+  
+  BOOST_CHECK(2 == g0.children.get().size());
+  BOOST_MESSAGE(g0 << '\n' << *g3 << '\n' << *g4 << '\n');
+}
