e6f27bbea288c759d6fdf42ddd7af422b62692bc
jan p springer <jsd@igroup.org>
2014-04-10 19:09:25 +0100

    added: preliminary scene:node:geometry implementation

--- inc/scene/node/geometry.hpp
+++ inc/scene/node/geometry.hpp
@@ -18,10 +18,12 @@
 
 // includes, system
 
-// #include <>
+#include <glm/glm.hpp> // glm::vec[3|4]
+#include <iosfwd>      // std::ostream (fwd)
 
 // includes, project
 
+#include <field/value_multi.hpp>
 #include <scene/node/base.hpp>
 
 namespace scene {
@@ -33,17 +35,57 @@ namespace scene {
     class geometry : public base {
 
     public:
+
+      typedef base subject_inherited;
+
+      struct attribute {
+
+      public:
+        
+        glm::vec3 position;
+        glm::vec3 normal;
+        glm::vec2 tcoord;
+        glm::vec4 tangent;
+        glm::vec4 color;
+
+        explicit attribute(glm::vec3 const& /* position */ = glm::vec3(0.0),
+                           glm::vec3 const& /* normal */   = glm::vec3(0.0),
+                           glm::vec2 const& /* tcoord */   = glm::vec2(0.0),
+                           glm::vec4 const& /* tangent */  = glm::vec4(0.0),
+                           glm::vec4 const& /* color */    = glm::vec4(1.0));
+        
+      };
+      
+      field::value::multi<attribute> attributes;
+      field::value::multi<unsigned>  indices;
+
+      virtual ~geometry() =0;
+
+      virtual void accept(visitor::base&);
+
+      void compute_bounds();
+      void compute_tangents();
+      
+    protected:
+
+      explicit geometry(std::string const& /* name */);
       
     };
     
     // variables, exported (extern)
 
     // functions, inlined (inline)
-  
+
+    bool operator==(geometry::attribute const&, geometry::attribute const&);
+    
     // functions, exported (extern)
 
+    std::ostream& operator<<(std::ostream&, geometry::attribute const&);
+    
   } // namespace node {
   
 } // namespace scene {
+
+#include <scene/node/geometry.inl>
 
 #endif // #if !defined(UKACHULLDCS_08961_SCENE_NODE_GEOMETRY_HPP)
--- src/lib/scene/node/geometry.cpp
+++ src/lib/scene/node/geometry.cpp
@@ -18,11 +18,12 @@
 
 // includes, system
 
-//#include <>
+#include <glm/gtx/io.hpp> // glm::operator<<
+#include <ostream>        // std::ostream
 
 // includes, project
 
-//#include <>
+#include <scene/visitor/base.hpp>
 
 #define UKACHULLDCS_USE_TRACE
 #undef UKACHULLDCS_USE_TRACE
@@ -47,6 +48,149 @@ namespace scene {
     // variables, exported
     
     // functions, exported
+
+    /* explicit */
+    geometry::attribute::attribute(glm::vec3 const& a, glm::vec3 const& b, glm::vec2 const& c,
+                                   glm::vec4 const& d, glm::vec4 const& e)
+      : position(a), normal(b), tcoord(c), tangent(d), color(e)
+    {
+      TRACE("scene::node::geometry::attribute::attribute");
+    }
+    
+    /* virtual */
+    geometry::~geometry()
+    {
+      TRACE("scene::node::geometry::~geometry");
+    }
+
+    /* virtual */ void
+    geometry::accept(visitor::base& v)
+    {
+      TRACE("scene::node::geometry::accept");
+
+      v.visit(*this);
+    }
+    
+    void
+    geometry::compute_bounds()
+    {
+      TRACE("scene::node::geometry::compute_bounds");
+
+      bounds b(bounds::invalid);
+      
+      if (!attributes.get().empty()) {
+        for (auto a : attributes.get()) {
+          // glm::m[ax|in] return component-wise max/min, i.e. min([1,0,0], [0,1,-1]) -> [0,0,-1]
+          b.min = glm::min(a.position, b.min);
+          b.max = glm::max(a.position, b.max);
+        }
+      } else {
+        b.min = b.max = glm::vec3(0.0);
+      }
+
+      b.valid = true;
+      
+      bbox = b;
+    }
+
+    /*
+     * see [http://www.terathon.com/code/tangent.html]
+     */
+    void
+    geometry::compute_tangents()
+    {
+      TRACE("scene::node::geometry::compute_tangents");
+
+      auto&       attr_list(attributes.get());
+      auto const& idx_list(indices.get());
+      
+      std::vector<std::pair<glm::vec3, glm::vec3>> tarray(idx_list.size());
+        
+      for (unsigned i(0); i < idx_list.size(); i += 3) {
+        unsigned const i1(idx_list[i+0]);
+        unsigned const i2(idx_list[i+1]);
+        unsigned const i3(idx_list[i+2]);
+        
+        glm::vec3 const& v1(attr_list[i1].position);
+        glm::vec3 const& v2(attr_list[i2].position);
+        glm::vec3 const& v3(attr_list[i3].position);
+
+        glm::vec2 const& w1(attr_list[i1].tcoord);
+        glm::vec2 const& w2(attr_list[i2].tcoord);
+        glm::vec2 const& w3(attr_list[i3].tcoord);
+        
+        float const x1(v2.x - v1.x);
+        float const x2(v3.x - v1.x);
+        float const y1(v2.y - v1.y);
+        float const y2(v3.y - v1.y);
+        float const z1(v2.z - v1.z);
+        float const z2(v3.z - v1.z);
+        
+        float const s1(w2.x - w1.x);
+        float const s2(w3.x - w1.x);
+        float const t1(w2.y - w1.y);
+        float const t2(w3.y - w1.y);
+        
+        float const     r   (1.0f / (s1 * t2 - s2 * t1));
+        glm::vec3 const sdir((t2 * x1 - t1 * x2) * r,
+                             (t2 * y1 - t1 * y2) * r,
+                             (t2 * z1 - t1 * z2) * r);
+        glm::vec3 const tdir((s1 * x2 - s2 * x1) * r,
+                             (s1 * y2 - s2 * y1) * r,
+                             (s1 * z2 - s2 * z1) * r);
+        
+        for (unsigned k(i); k < i + 3; ++k) {
+          tarray[k].first  += sdir;
+          tarray[k].second += tdir;
+        }
+      }
+
+#if 0
+      for (auto idx : idx_list) {
+        auto&       a(attr_list[idx]);
+        auto const& t(tarray[idx]);
+        
+        a.tangent.xyz = glm::normalize(t.first - a.normal * glm::dot(a.normal, t.first));
+        a.tangent.w   = (0.0 > glm::dot(glm::cross(a.normal, t.first), t.second)) ? -1.0f : 1.0f;
+      }
+#else
+#  pragma message "please fix 'scene::node::geometry::compute_tangents' implementation"
+#endif
+    }
+
+    /* explicit */
+    geometry::geometry(std::string const& a)
+      : base      (a),
+        attributes(*this, "attributes"),
+        indices   (*this, "indices")
+    {
+      TRACE("scene::node::geometry::geometry");
+    }
+    
+    std::ostream&
+    operator<<(std::ostream& os, geometry::attribute const& a)
+    {
+      TRACE_NEVER("scene::node::operator<<(geometry::attribute const&)");
+      
+      std::ostream::sentry const cerberus(os);
+
+      if (cerberus) {
+        glm::io::precision_guard const iopg;
+        
+        glm::io::precision()   = 2;
+        glm::io::value_width() = 1 + 2 + 1 + glm::io::precision();
+        
+        os << '['
+           << "v:"  << a.position << ','
+           << "n:"  << a.normal   << ','
+           << "tc:" << a.tcoord   << ','
+           << "t:"  << a.tangent  << ','
+           << "c:"  << a.color
+           << ']';
+      }
+
+      return os;
+    }
     
   } // namespace node {
   
