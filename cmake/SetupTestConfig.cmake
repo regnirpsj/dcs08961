b9d5c3f2a0d7bf888cfd05fbe43613c7fe7a536c
regnirpsj <regnirpsj@users.noreply.github.com>
2014-04-14 22:08:03 +0100

    Update SetupTestConfig.cmake

--- cmake/SetupTestConfig.cmake
+++ cmake/SetupTestConfig.cmake
@@ -6,8 +6,8 @@
 #                                                                                                  #
 ####################################################################################################
 
-SET(CTEST_PROJECT_NAME    "DCSC08961")
+SET(CTEST_PROJECT_NAME    "DCS08961")
 SET(CTEST_DROP_METHOD     "http")
 SET(CTEST_DROP_SITE       "www.cdash.org")
-SET(CTEST_DROP_LOCATION   "/CDash/submit.php?project=DCSC08961")
+SET(CTEST_DROP_LOCATION   "/CDash/submit.php?project=DCS08961")
 SET(CTEST_DROP_SITE_CDASH TRUE)
