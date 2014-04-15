3bee3bd2e7696d39a497a2ff75ffe97e7aff3cbc
jan p springer <jsd@igroup.org>
2014-04-15 02:10:27 +0100

    cleanup

--- inc/support/thread.hpp
+++ inc/support/thread.hpp
@@ -18,9 +18,8 @@
 
 // includes, system
 
-#include <condition_variable> // std::condition_variable
 #include <iosfwd>             // std::ostream (fwd)
-#include <mutex>              // std::mutex, std::timed_mutex, std::unique_lock<>
+#include <mutex>              // std::mutex, std::lock_guard<>
 #include <string>             // std::string
 #include <thread>             // std::this_thread, std::thread
 
@@ -29,7 +28,8 @@
 #include <support/chrono.hpp> // support::clock::duration
 
 #if defined(__GNUC__)
-#  define thread_local __thread
+// 'thread_local' <> '__thread'
+// #  define thread_local __thread
 #elif defined(_MSC_VER)
 #  define thread_local __declspec(thread)
 #else
@@ -42,19 +42,8 @@ namespace support {
 
   namespace this_thread = std::this_thread;
   
-  typedef std::thread                    thread;
-  
-  typedef std::mutex                     simple_lock;
-  typedef std::unique_lock<simple_lock>  simple_lock_guard;
-
-  typedef std::timed_mutex               timed_lock;
-  typedef std::unique_lock<timed_lock>   timed_lock_guard;
-
-  // typedef std::shared_mutex              rw_lock;
-  // typedef std::shared_lock<rw_lock>      read_lock_guard;
-  // typedef std::unique_lock<rw_lock>      write_lock_guard;
-
-  typedef std::condition_variable        condition_variable;
+  typedef std::mutex                   simple_lock;
+  typedef std::lock_guard<simple_lock> simple_lock_guard;
   
   // variables, exported (extern)
 
