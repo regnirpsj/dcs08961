729241f6fb5026b0b1f9e46ed9bb1ad9a8fa9763
jan p springer <jsd@igroup.org>
2014-04-15 13:06:15 +0100

    added: application skeleton

---
+++ src/app/projection/main.cpp
@@ -0,0 +1,43 @@
+// -*- Mode:C++ -*-
+
+/**************************************************************************************************/
+/*                                                                                                */
+/* Copyright (C) 2014 University of Hull                                                          */
+/*                                                                                                */
+/**************************************************************************************************/
+/*                                                                                                */
+/*  module     :  projection/main.cpp                                                             */
+/*  project    :                                                                                  */
+/*  description:                                                                                  */
+/*                                                                                                */
+/**************************************************************************************************/
+
+// includes, system
+
+#include <cstdlib> // EXIT_SUCCESS
+
+// includes, project
+
+//#include <>
+
+#define UKACHULLDCS_USE_TRACE
+#undef UKACHULLDCS_USE_TRACE
+#include <support/trace.hpp>
+
+// internal unnamed namespace
+
+namespace {
+  
+  // types, internal (class, enum, struct, union, typedef)
+
+  // variables, internal
+  
+  // functions, internal
+
+} // namespace {
+
+int
+main(int /* argc */, char* /* argv */[])
+{
+  return EXIT_SUCCESS;
+}
