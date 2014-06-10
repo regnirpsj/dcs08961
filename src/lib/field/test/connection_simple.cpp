d120b2be1ba3b7d66fcce8c8300231a822aa58f5
jan p springer <jsd@igroup.org>
2014-06-10 09:40:20 +0100

    update

--- src/lib/field/test/connection_simple.cpp
+++ src/lib/field/test/connection_simple.cpp
@@ -42,29 +42,79 @@ namespace {
 BOOST_AUTO_TEST_CASE_TEMPLATE(test_field_connection_simple_single, T, field::test::single_types)
 {
   using namespace field;
-  
-  test::container_single<T>            c;
-  value::single<T> const               f1(c, "f1");
-  value::single<T>                     f2(c, "f2");
-  connection::simple<value::single<T>> r (f1, f2);
 
-  r.update();
+  {
+    test::container_single<T>            c;
+    value::single<T> const               f1(c, "f1");
+    value::single<T>                     f2(c, "f2");
+    connection::simple<value::single<T>> r (f1, f2);
+
+    r.update();
   
-  BOOST_CHECK(f2.get() == f1.get());
-  BOOST_MESSAGE(r);
+    BOOST_CHECK(f2.get() == f1.get());
+    BOOST_MESSAGE(&r << ':' << r);
+  }
+
+  {
+    test::container_single<T> c;
+    value::single<T>          f1(c, "f1");
+    value::single<T>          f2(c, "f2");
+
+    typedef connection::simple<value::single<T>> connection_type;
+
+    connection_type* r(new connection_type(f1, f2));
+    
+    BOOST_CHECK(f1.connection_add(r));
+    BOOST_CHECK(!f1.connection_add(r));
+    
+    f1.touch();
+    
+    BOOST_CHECK(f2.get() == f1.get());
+
+    BOOST_MESSAGE(r << ':' << *r);
+
+    BOOST_CHECK(f1.connection_sub(r));
+
+    delete r;
+  }
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(test_field_connection_simple_multi, T, field::test::multi_types)
 {
   using namespace field;
-  
-  test::container_multi<T>            c;
-  value::multi<T> const               f1(c, "f1");
-  value::multi<T>                     f2(c, "f2");
-  connection::simple<value::multi<T>> r (f1, f2);
 
-  r.update();
+  {
+    test::container_multi<T>            c;
+    value::multi<T> const               f1(c, "f1");
+    value::multi<T>                     f2(c, "f2");
+    connection::simple<value::multi<T>> r (f1, f2);
+
+    r.update();
   
-  BOOST_CHECK(f2.get() == f1.get());
-  BOOST_MESSAGE(r);
+    BOOST_CHECK(f2.get() == f1.get());
+    BOOST_MESSAGE(&r << ':' << r);
+  }
+
+  {
+    test::container_multi<T> c;
+    value::multi<T>          f1(c, "f1");
+    value::multi<T>          f2(c, "f2");
+
+    typedef connection::simple<value::multi<T>> connection_type;
+
+    connection_type* r(new connection_type(f1, f2));
+    
+    BOOST_CHECK(f1.connection_add(r));
+    BOOST_CHECK(!f1.connection_add(r));
+    
+    f1.touch();
+    
+    BOOST_CHECK(f2.get() == f1.get());
+
+    BOOST_MESSAGE(r << ':' << *r);
+
+    BOOST_CHECK(f1.connection_sub(r));
+
+    delete r;
+  }
 }
