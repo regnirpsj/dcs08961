fed625e12c8a744f486c571a132a63cbc7f59fb5
jan p springer <jsd@igroup.org>
2014-07-09 18:18:15 +0100

    fixed: typo

--- cmake/SetupProjectOptions.cmake
+++ cmake/SetupProjectOptions.cmake
@@ -7,15 +7,15 @@
 ####################################################################################################
 
 # glibcxx parallel support
-OPTION(DCSC08961_GLIBCXX_PARALLEL "Enable GLIBCXX parallel mode" OFF)
+OPTION(DCS08961_GLIBCXX_PARALLEL "Enable GLIBCXX parallel mode" OFF)
 
 # unit testing
-OPTION(DCSC08961_BLD_UTEST "Enable unit-test building by default"                          ON)
-OPTION(DCSC08961_RUN_CTEST "Enable unit-test execution using CTest by default"             ON)
-OPTION(DCSC08961_RUN_UTEST "Enable unit-test execution using the build system by default" OFF)
+OPTION(DCS08961_BLD_UTEST "Enable unit-test building by default"                          ON)
+OPTION(DCS08961_RUN_CTEST "Enable unit-test execution using CTest by default"             ON)
+OPTION(DCS08961_RUN_UTEST "Enable unit-test execution using the build system by default" OFF)
 
 # coverage testing
-OPTION(DCSC08961_PROFILE   "Enable support for coverage testing" OFF)
+OPTION(DCS08961_PROFILE   "Enable support for coverage testing" OFF)
 
 # tracing
-OPTION(DCSC08961_TRACE_ALL "Enable runtime function tracing for everything!" OFF)
+OPTION(DCS08961_TRACE_ALL "Enable runtime function tracing for everything!" OFF)
