350245015b9a02b600be3c39e77edb85fc99b501
jan p springer <jsd@igroup.org>
2014-07-24 08:54:58 +0100

    added: generic functions

--- src/app/oglplus/glsl/app_oglplus_02.light.glsl
+++ src/app/oglplus/glsl/app_oglplus_02.light.glsl
@@ -26,6 +26,7 @@
 /* includes, project */
 
 #include <app_oglplus_02.constants.glsl>
+#include <app_oglplus_02.functions.glsl>
 
 /* constants */
 
@@ -61,21 +62,6 @@ layout (std430) buffer light_list_t {
 /* variables, global */
 
 /* functions */
-
-vec4
-lit(in float NdotL, in float NdotH, in float m)
-{ 
-  float ambient  = 1.0;
-  float diffuse  = max(NdotL, 0.0);
-  float specular = step(0.0, NdotL) * max(NdotH * m, 0.0);
- 
-  return vec4(ambient, diffuse, specular, 1.0);
-}
-
-float saturate(in float a) { return clamp(a, 0.0, 1.0); }
-vec2  saturate(in vec2 a)  { return clamp(a, 0.0, 1.0); }
-vec3  saturate(in vec3 a)  { return clamp(a, 0.0, 1.0); }
-vec4  saturate(in vec4 a)  { return clamp(a, 0.0, 1.0); }
 
 vec3
 light_vector(in light_t l, in vec3 P)
