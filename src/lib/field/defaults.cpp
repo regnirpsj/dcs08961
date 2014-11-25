a6e659823d4c6dd51f966b0b98521336370112d7
jan p springer <jsd@igroup.org>
2014-11-25 22:56:51 +0000

    disabled default field specialization for built-ins

--- src/lib/field/defaults.cpp
+++ src/lib/field/defaults.cpp
@@ -35,28 +35,32 @@ namespace {
 
 // variables, exported
 
-#define DEFAULT_FIELD_TYPE_FOR(type)           \
+#if 0 // what for?
+
+#define DEFAULT_FIELD_TYPES_FOR(type)           \
   template class field::value::multi   <type>; \
   template class field::value::single  <type>; \
   template class field::adapter::multi <type>; \
   template class field::adapter::single<type>
 
-DEFAULT_FIELD_TYPE_FOR(         bool);
-DEFAULT_FIELD_TYPE_FOR(signed   char);
-DEFAULT_FIELD_TYPE_FOR(unsigned char);
-DEFAULT_FIELD_TYPE_FOR(         char16_t);
-DEFAULT_FIELD_TYPE_FOR(         char32_t);
-DEFAULT_FIELD_TYPE_FOR(signed   short);
-DEFAULT_FIELD_TYPE_FOR(unsigned short);
-DEFAULT_FIELD_TYPE_FOR(signed   int);
-DEFAULT_FIELD_TYPE_FOR(unsigned int);
-DEFAULT_FIELD_TYPE_FOR(signed   long int);
-DEFAULT_FIELD_TYPE_FOR(unsigned long int);
-DEFAULT_FIELD_TYPE_FOR(signed   long long int);
-DEFAULT_FIELD_TYPE_FOR(unsigned long long int);
-DEFAULT_FIELD_TYPE_FOR(         float);
-DEFAULT_FIELD_TYPE_FOR(         double);
-DEFAULT_FIELD_TYPE_FOR(         long double);
-DEFAULT_FIELD_TYPE_FOR(         std::string);
+DEFAULT_FIELD_TYPES_FOR(         bool);
+DEFAULT_FIELD_TYPES_FOR(signed   char);
+DEFAULT_FIELD_TYPES_FOR(unsigned char);
+DEFAULT_FIELD_TYPES_FOR(         char16_t);
+DEFAULT_FIELD_TYPES_FOR(         char32_t);
+DEFAULT_FIELD_TYPES_FOR(signed   short);
+DEFAULT_FIELD_TYPES_FOR(unsigned short);
+DEFAULT_FIELD_TYPES_FOR(signed   int);
+DEFAULT_FIELD_TYPES_FOR(unsigned int);
+DEFAULT_FIELD_TYPES_FOR(signed   long int);
+DEFAULT_FIELD_TYPES_FOR(unsigned long int);
+DEFAULT_FIELD_TYPES_FOR(signed   long long int);
+DEFAULT_FIELD_TYPES_FOR(unsigned long long int);
+DEFAULT_FIELD_TYPES_FOR(         float);
+DEFAULT_FIELD_TYPES_FOR(         double);
+DEFAULT_FIELD_TYPES_FOR(         long double);
+DEFAULT_FIELD_TYPES_FOR(         std::string);
 
-#undef DEFAULT_FIELD_TYPE_FOR
+#undef DEFAULT_FIELD_TYPES_FOR
+
+#endif // #if 0
