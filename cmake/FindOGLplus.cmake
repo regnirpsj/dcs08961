ee43493160ca6a17c972d1cce09de2dfea280b37
jan p springer <jsd@igroup.org>
2015-04-04 21:18:57 +0100

    update

--- cmake/FindOGLplus.cmake
+++ cmake/FindOGLplus.cmake
@@ -2,7 +2,7 @@
 
 ####################################################################################################
 #                                                                                                  #
-# Copyright (C) 2014 University of Hull                                                            #
+# Copyright (C) 2014-2015 University of Hull                                                       #
 #                                                                                                  #
 ####################################################################################################
 
@@ -25,7 +25,7 @@ ENDIF()
 
 # non-installed build-only locations will have .hpp/.ipp files in different directories;
 # in case of an installed location, i.e. .hpp/.ipp in the same place, the following
-# 'LIST(REMOVE_DUPLICATES ...)' takes care of dublicate
+# 'LIST(REMOVE_DUPLICATES ...)' takes care of duplicate
 FIND_PATH(OGLplus_HPP_DIR "oglplus/buffer.hpp"
           PATHS ${_oglplus_HEADER_SEARCH_DIRS}
 	  DOC "The directory where oglplus/buffer.hpp resides")
@@ -34,13 +34,15 @@ FIND_PATH(OGLplus_IPP_DIR "oglplus/buffer.ipp"
           PATHS ${_oglplus_HEADER_SEARCH_DIRS}
 	  DOC "The directory where oglplus/buffer.ipp resides")
 
-SET(OGLplus_INCLUDE_DIRS "${OGLplus_HPP_DIR};${OGLplus_IPP_DIR}")
+SET(OGLplus_INCLUDE_DIRS)
+LIST(APPEND OGLplus_INCLUDE_DIRS "${OGLplus_HPP_DIR}")
+LIST(APPEND OGLplus_INCLUDE_DIRS "${OGLplus_IPP_DIR}")
 LIST(REMOVE_DUPLICATES OGLplus_INCLUDE_DIRS)
 
 INCLUDE(FindPackageHandleStandardArgs)
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(OGLplus DEFAULT_MSG OGLplus_INCLUDE_DIRS)
 
-IF(OGLplus_FOUND AND VERBOSE)
+IF(OGLPLUS_FOUND AND VERBOSE)
   MESSAGE(STATUS "OGLplus_INCLUDE_DIRS = ${OGLplus_INCLUDE_DIRS}")
 ENDIF()
