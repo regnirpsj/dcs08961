3d1c6db11fe0a9b84cf2ec0f8cd274391dc6ff7c
jsd@igroup.org <jsd@igroup.org>
2015-04-21 23:35:12 +0100

    update

--- cmake/DCS08961_SetupVariables.cmake
+++ cmake/DCS08961_SetupVariables.cmake
@@ -17,6 +17,17 @@ if(CMAKE_HOST_WIN32)
   set(AntTweakBar_ROOT_DIR "C:/Tools/anttweakbar/1.16")
 endif()
 
+# Freeglut
+set(FREEGLUT_MINIMUM_VERSION "2.8")
+if(CMAKE_HOST_WIN32)
+  set(FreeGLUT_ROOT_DIR "C:/Tools/freeglut/3.0.0-src/install/${ARCH}")
+endif()
+
+# GLEW
+if(CMAKE_HOST_WIN32)
+  set(GLEW_ROOT_DIR "C:/Tools/glew/1.12.0")
+endif()
+
 # GL[I|M]
 set(GLI_MINIMUM_VERSION "0.6.0")
 set(GLM_MINIMUM_VERSION "0.9.7")
@@ -28,27 +39,13 @@ elseif(UNIX)
   set(GLM_ROOT_DIR "~/Projects/others/glm-git")
 endif()
 
-# freeglut
-set(FREEGLUT_MINIMUM_VERSION "2.8")
-if(CMAKE_HOST_WIN32)
-  set(FreeGLUT_ROOT_DIR "C:/Tools/freeglut/3.0.0-src/install/${ARCH}")
-endif()
-
 # OGLplus
 if(CMAKE_HOST_WIN32)
-  set(OGLplus_ROOT_DIR "C:/Tools/oglplus/oglplus-git")
+  set(OGLplus_ROOT_DIR "C:/Tools/oglplus/oglplus-git/_install")
 else()
   if(UNIX)
     set(OGLplus_ROOT_DIR "~/Projects/others/oglplus-develop-git/_install")
   endif()
-endif()
-
-# GLEW
-# keeping this last because of 'CMAKE_*_PATH' tinkering
-if(CMAKE_HOST_WIN32)
-  set(GLEW_ROOT_PATH "C:/Tools/glew/1.12.0")
-  set(CMAKE_PREFIX_PATH  "${GLEW_ROOT_PATH}")
-  set(CMAKE_LIBRARY_PATH "${GLEW_ROOT_PATH}/lib/Release/${ARCH}")
 endif()
 
 if(DCS08961_TRACE_ALL)
