7bb30572d42c6294779aa9cc982b8db3e7a74d21
jsd@igroup.org <jsd@igroup.org>
2015-04-22 08:07:34 +0100

    update

--- inc/platform/win32/window/simple.hpp
+++ inc/platform/win32/window/simple.hpp
@@ -25,17 +25,17 @@
 #include <platform/win32/window/base.hpp>
 
 namespace platform {
-
+  
   namespace win32 {
-
+    
     namespace window {
       
       // types, exported (class, enum, struct, union, typedef)
-
+      
       class DCS08961_PLATFORM_EXPORT simple : public platform::win32::window::base {
         
       public:
-      
+        
         using rect = platform::window::rect;
         
         field::value::single<bool> focused;
@@ -43,29 +43,29 @@ namespace platform {
         
         explicit simple(std::string const& /* title */,
                         rect const&        /* rect */ = rect::dflt_rect);
-
+        
         virtual ~simple();
         
       protected:
-      
+        
         virtual LRESULT CALLBACK cb_window_proc(HWND, UINT, WPARAM, LPARAM);
-      
+        
         virtual bool focus (bool /* focused or not */);
         virtual bool redraw();
         virtual bool resize(glm::ivec2 const& /* size */, bool /* minimized */);
-      
+        
       };
       
       // variables, exported (extern)
-
+      
       // functions, inlined (inline)
-  
+      
       // functions, exported (extern)
-
+      
     } // namespace window {
-        
-  } // namespace win32 {
     
+  } // namespace win32 {
+  
 } // namespace platform {
 
 #endif // #if !defined(UKACHULLDCS_08961_PLATFORM_WIN32_WINDOW_SIMPLE_HPP)
