7b3b7ba390f0b407ba12e4d2e1fa6d187d4e2009
jsd@igroup.org <jsd@igroup.org>
2015-04-23 15:35:33 +0100

    added: helper for things missing in winrt

---
+++ src/lib/platform/winrt/addons/getenv.c
@@ -0,0 +1,37 @@
+/* -*- Mode:C -*- */
+
+/**************************************************************************************************/
+/*                                                                                                */
+/* Copyright (C) 2015 University of Hull                                                          */
+/*                                                                                                */
+/**************************************************************************************************/
+/*                                                                                                */
+/*  module     :  platform/winrt/addons/getenv.c                                                  */
+/*  project    :                                                                                  */
+/*  description:                                                                                  */
+/*                                                                                                */
+/**************************************************************************************************/
+
+/* includes, system */
+
+/* #include <> */
+
+/* includes, project */
+
+#include <platform/export.h>
+  
+/* types, internal (enum, struct, union, typedef) */
+
+/* variables, internal (static) */
+  
+/* functions, internal (static) */
+
+/* variables, exported */
+  
+/* functions, exported */
+
+DCS08961_PLATFORM_EXPORT char*
+_imp__getenv(char const* unused)
+{
+  return 0;
+}
