e96607a5aa6eaaa9693ae6436998fd8efc612793
jan p springer <jsd@igroup.org>
2015-07-08 05:07:30 +0100

    preliminary version

--- src/lib/scene/loader/xform.cpp
+++ src/lib/scene/loader/xform.cpp
@@ -19,15 +19,16 @@
 // includes, system
 
 #include <boost/intrusive_ptr.hpp> // boost::intrusive_ptr<>
-#include <boost/tokenizer.hpp>     // boost::char_separator<>, boost::tokenizer<>
+#include <cctype>                  // std::isdigit
 
 // includes, project
 
 #include <scene/file.hpp>
 #include <scene/node/transform.hpp>
+#include <support/io_utils.hpp>
 
 #define UKACHULLDCS_USE_TRACE
-#undef UKACHULLDCS_USE_TRACE
+//#undef UKACHULLDCS_USE_TRACE
 #include <support/trace.hpp>
 
 // internal unnamed namespace
@@ -45,33 +46,123 @@ namespace {
   {
     TRACE("scene::file::xform::<unnamed>::process_xform");
 
-    static boost::char_separator<char> const token_separator(".");
-          
-    using tokenizer = boost::tokenizer<boost::char_separator<char>>;
+    using size_type       = std::string::size_type;
+    using pos_list_type   = std::vector<size_type>;
+    using float_list_type = std::vector<float>;
     
-    tokenizer tokens(a, token_separator);
+    pos_list_type dots;
+    pos_list_type commas;
 
+    for (size_type i(0); i < a.size(); ++i) {
+      if      ('.' == a[i]) { dots  .push_back(i); }
+      else if (',' == a[i]) { commas.push_back(i); }
+    }
+
+    for (size_type i(commas.size()-1); i > 0; --i) {
+      unsigned count(0);
+
+      for (auto const& d : dots) {
+        if ((d < commas[i]) && (d > commas[i-1])) {
+          ++count;
+        }
+      }
+  
+      std::cout << support::trace::prefix()
+                << commas[i] << ':' << commas[i-1] << ':' << count
+                << '\n';
+
+      if (2 <= count) {
+        count = 0;
+        
+        pos_list_type tmp;
+
+        for (auto const& d : dots) {
+          if (d > commas[i-1]) {
+            ++count;
+
+            if (2 <= count) {
+              tmp.push_back(d);
+            }
+          }
+        }
+        
+        std::swap(tmp, dots);
+      }
+    }
+    
+    pos_list_type ranges(commas);
+
+    ranges.insert   (ranges.begin(), *dots.begin());
+    ranges.push_back(*dots.rbegin());
+
+    for (size_type i(*commas.begin()); i > *dots.begin(); --i) {
+      if (('.' == a[i]) &&
+          (std::isdigit(a[i+1]) || ('-' == a[i+1]) || ('+' == a[i+1])) &&
+          (!std::isdigit(a[i-1]) && ('-' != a[i-1]) && ('+' != a[i-1]))) {
+        ranges[0] = i;
+      }
+    }
+
+    float_list_type values;
+    
+    for (size_type i(0); i < ranges.size() - 1; ++i) {
+      std::string const tmp(a, ranges[i]+1, ranges[i+1]-ranges[i]-1);
+      float             f(0.0f);
+      
+      if (1 == std::sscanf(tmp.c_str(), "%f", &f)) {
+        values.push_back(f);
+      }
+
+#if 0
+      std::cout << support::trace::prefix()
+                << ranges[i] << ":'" << tmp << "' -> " << f
+                << '\n';
+#endif
+    }
+    
     using scene::node::transform;
     
-    transform* result(new transform);
+    transform*        result(new transform);
+    std::string const suffix(a.substr(*dots.rbegin()));
     
-    // remove '.<float>,<float>,<float>,<float>.rot'
-    // remove '.<float>,<float>,<float>.scale'
-    // remove '.<float>,<float>,<float>,<float>.trans'
-    // apply xform
-
-    if        ("rot"   == *tokens.begin()) {
-    } else if ("scale" == *tokens.begin()) {
-    } else if ("trans" == *tokens.begin()) {
+    if        (".rot"   == suffix) {
+      using support::ostream::operator<<;
+      
+      std::cout << support::trace::prefix() << "ROT: " << values << "\n";
+    } else if (".scale" == suffix) {
+      using support::ostream::operator<<;
+      
+      std::cout << support::trace::prefix() << "SCALE: " << values << "\n";
+    } else if (".trans" == suffix) {
+      using support::ostream::operator<<;
+      
+      std::cout << support::trace::prefix() << "TRANS: " << values << "\n";
     } else {
       result->xform = glm::mat4(1);
     }
+
+    std::string tmp = a.substr(0, *ranges.begin());
+
+    {
+      using support::ostream::operator<<;
+      
+      std::cout << support::trace::prefix() << "'" << a << "' -> '" << tmp << "'"
+                << "\n"
+                << support::trace::prefix() << "d:" << dots << ", c:" << commas << ", r:" << ranges
+        //      << '\n'
+        //      << support::trace::prefix() << values
+                << '\n';
+    }
+
+    a = tmp;
     
     return result;
   }
   
 } // namespace {
 
+//dummy.suffix.1.0,2.0,3.0,4.0.rot
+                                                                               
 namespace scene {
 
   namespace file {
